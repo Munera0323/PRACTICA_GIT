7e7aa048e660f7bd1d3a3998ec9be330480835dd
Munera0323 <muneragarcia123@gmail.com>
2025-04-30 19:34:06 -0500

    inicio proyecto para la primeraRama

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,10 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("hola!");
+        System.out.printf("como estas!");
+        System.out.printf("bien!");
+
+
 
 
         }
